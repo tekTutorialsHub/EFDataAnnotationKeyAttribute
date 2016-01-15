51648aa966ddc7438c77455b98b0522a4731f529
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-15 22:15:23 +0530

    Created

---
+++ EFDataAnnotationKeyAttribute/EFContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EFDataAnnotationKeyAttribute
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base("EFDatabase")
+        {
+
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseAlways<EFContext>());
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Employee1> Employees1 { get; set; }
+        public DbSet<Employee2> Employees2 { get; set; }
+        public DbSet<Employee3> Employees3 { get; set; }
+        public DbSet<Employee4> Employees4 { get; set; }
+        //public DbSet<Employee5> Employees5 { get; set; }
+
+    }
+
+
+}
---
+++ EFDataAnnotationKeyAttribute/models.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFDataAnnotationKeyAttribute
+{
+
+    //Code First Default Convention. EmployeeID is Primary key
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    //EmployeeCode is Primary Key and identity Column
+    public class Employee1
+    {
+        public int EmployeeID { get; set; }
+        [Key]
+        public int EmployeeCode { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    //EmployeeCode is Primary Key.
+
+    public class Employee2
+    {
+        public int EmployeeID { get; set; }
+        [Key]
+        public string EmployeeCode { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    //EmployeeCode is Primary Key. Identity is disabled
+    public class Employee3
+    {
+        public int EmployeeID { get; set; }
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int EmployeeCode { get; set; }
+        public string Name { get; set; }
+    }
+
+
+
+    public class Employee4
+    {
+        public int EmployeeID { get; set; }
+
+        public int EmployeeNo { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public int EmployeeKey1 { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int EmployeeKey2 { get; set; }
+    }
+
+
+
+
+}
